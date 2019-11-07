a084f0108e8f078bfe442cf8bc13d0058f9cf028
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-11-07 17:43:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['numpy',
 		 'scipy',
 		'pystoi @ https://github.com/schmiph2/pystoi/archive/master.zip#egg=pystoi',
-		'pypesq @ https://github.com/schmiph2/python-pesq/archive/master.zip#egg=pypesq',
+		'pypesq @ https://github.com/schmiph2/python-pesq/archive/master.zip',
 	], 
     classifiers=[
         'Development Status :: 4 - Beta',
