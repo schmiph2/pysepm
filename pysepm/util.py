1b4db91be243552a4d4834ee04b471e66062eead
schmiph2 <philipp-m.schmid@bluewin.ch>
2020-06-17 18:15:42 +0300

    init commit

---
+++ pysepm/util.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def extract_overlapped_windows(x,nperseg,noverlap,window=None):
+    # source: https://github.com/scipy/scipy/blob/v1.2.1/scipy/signal/spectral.py
+    step = nperseg - noverlap
+    shape = x.shape[:-1]+((x.shape[-1]-noverlap)//step, nperseg)
+    strides = x.strides[:-1]+(step*x.strides[-1], x.strides[-1])
+    result = np.lib.stride_tricks.as_strided(x, shape=shape,
+                                             strides=strides)
+    if window is not None:
+        result = window * result
+    return result
