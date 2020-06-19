a734d0f8be2802e2a3f6e3534a638c07f70174f1
Schmid Philipp Matthias <scdp@gpro1.cloudlab.zhaw.ch>
2020-06-19 15:08:38 +0200

    new metric added to init

--- pysepm/__init__.py
+++ pysepm/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '0.1'
 
 
-from .qualityMeasures import fwSNRseg,SNRseg,llr,wss,composite,pesq
+from .qualityMeasures import fwSNRseg,SNRseg,llr,wss,composite,pesq,cepstrum_distance
 from .intelligibilityMeasures import stoi,csii,ncm
 from .reverberationMeasures import srr_seg,rdt,bsd,srmr
 
