051c6af37eb8add7d8b8819bf8d88fd6f20ffb3f
Schmid Philipp Matthias <scdp@gpro1.cloudlab.zhaw.ch>
2020-06-23 14:10:39 +0200

    prototype v2 of tets

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,8 +7,8 @@ import pytest
 import numpy.testing
 import librosa
 
-RTOL = 1e-5
-ATOL = 1e-4
+RTOL = 1e-3
+ATOL = 1e-3
 
 resultsOrig = loadmat("results.mat")
 resultsOrig=resultsOrig['results']
@@ -37,21 +37,16 @@ testScenarios.append((['cleanSample_valIdx356.wav','enhancedSample_valIdx356.wav
 def load_preprocess_filepair(filePair,resample=False,fs_targ=16000):
     fs, cleanSig = wavfile.read('data/'+filePair[0])
     fs, enhancedSig = wavfile.read('data/'+filePair[1])
-    if cleanSig.dtype=='int16':
-        cleanSig = cleanSig.astype('float')/abs(np.iinfo(cleanSig.dtype).min)
-    if enhancedSig.dtype=='int16':
-        enhancedSig = enhancedSig.astype('float')/abs(np.iinfo(enhancedSig.dtype).min)
-    
-    if cleanSig.shape!=enhancedSig.shape:
-        minLen=min((len(cleanSig),len(enhancedSig)))
-        cleanSig = cleanSig[0:minLen]
-        enhancedSig = enhancedSig[0:minLen]
-    cleanSig = cleanSig/np.max(np.abs(cleanSig))
-    enhancedSig = enhancedSig/np.max(np.abs(enhancedSig))
-    
+        
     if resample:
-        fs, cleanSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[0])
-        fs, enhancedSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[1])
+        if fs_targ == 16000 and fs !=8000:
+            fs, cleanSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[0])
+            fs, enhancedSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[1])
+        elif fs_targ == 16000 and fs == 8000:
+            pass
+        else:
+            fs, cleanSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[0])
+            fs, enhancedSig = wavfile.read('data/'+str(int(fs_targ/1000))+'kHz_'+filePair[1])
 
         #cleanSig=librosa.core.resample(cleanSig, fs,fs_targ)
         #enhancedSig=librosa.core.resample(enhancedSig,fs,fs_targ)        
@@ -90,7 +85,7 @@ def test_stoi(filePair,expected_vals):
     
     numpy.testing.assert_allclose(pm.stoi(cleanSig, enhancedSig, fs), expected_vals[5], rtol=RTOL, atol=ATOL)
     
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios) 
 def test_csii(filePair,expected_vals):
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair)
     CSIIh,CSIIm,CSIIl=pm.csii(cleanSig, enhancedSig, fs)
@@ -99,13 +94,13 @@ def test_csii(filePair,expected_vals):
     numpy.testing.assert_allclose(CSIIl, expected_vals[8], rtol=RTOL, atol=ATOL)
 
     
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios) 
 def test_pesq(filePair,expected_vals):
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair,True,16e3)
-    mos_lqo, pesq_mos=pm.pesq(cleanSig, enhancedSig, fs)
+    pesq_mos,mos_lqo=pm.pesq(cleanSig, enhancedSig, fs)
     numpy.testing.assert_allclose(mos_lqo, expected_vals[10], rtol=RTOL, atol=ATOL)
 
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios) 
 def test_composite(filePair,expected_vals):
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair,True,16e3)
     Csig,Cbak,Covl=pm.composite(cleanSig, enhancedSig, fs)
@@ -113,21 +108,23 @@ def test_composite(filePair,expected_vals):
     numpy.testing.assert_allclose(Cbak, expected_vals[12], rtol=RTOL, atol=ATOL)
     numpy.testing.assert_allclose(Covl, expected_vals[13], rtol=RTOL, atol=ATOL)
     
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios)
 def test_ncm(filePair,expected_vals):
+    rto = 5e-3
+    atol = 2e-3 # higher tolerances due to differences between the used resampling method und the one of matlab
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair,True,16e3)
-    numpy.testing.assert_allclose(pm.ncm(cleanSig, enhancedSig, fs), expected_vals[14], rtol=RTOL, atol=ATOL)
+    numpy.testing.assert_allclose(pm.ncm(cleanSig, enhancedSig, fs), expected_vals[14], rtol=rto, atol=atol)
 
     
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios)
 def test_srmr(filePair,expected_vals):
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair,True,16e3)
     ratio,energy = pm.srmr(enhancedSig, fs)
     numpy.testing.assert_allclose(ratio, expected_vals[15], rtol=RTOL, atol=ATOL)
 
     
-@pytest.mark.parametrize('filePair,expected_vals', testScenarios) # needs 10 kHz samping rate!
+@pytest.mark.parametrize('filePair,expected_vals', testScenarios)
 def test_bsd(filePair,expected_vals):
     cleanSig,enhancedSig,fs=load_preprocess_filepair(filePair)
-    ratio,energy = pm.bsd(cleanSig, enhancedSig, fs)
-    numpy.testing.assert_allclose(ratio, np.inf, rtol=RTOL, atol=ATOL)
+    bsd = pm.bsd(cleanSig, enhancedSig, fs)
+    numpy.testing.assert_allclose(bsd, 1e20, rtol=RTOL, atol=ATOL)
