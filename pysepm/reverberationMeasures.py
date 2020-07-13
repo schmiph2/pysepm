b0967e6f3fcde2e01d108080e039d42445d2b9ef
schmiph2 <philipp-m.schmid@bluewin.ch>
2020-07-13 12:28:37 +0300

    removed energy output of srmr

--- pysepm/reverberationMeasures.py
+++ pysepm/reverberationMeasures.py
@@ -9,15 +9,17 @@ def srr_seg(clean_speech, processed_speech,fs):
 
 def srmr(speech,fs, n_cochlear_filters=23, low_freq=125, min_cf=4, max_cf=128, fast=False, norm=False):    
     if fs == 8000:
-        return srmrpy.srmr(speech, fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
+        srmRatio,energy=srmrpy.srmr(speech, fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
+        return srmRatio
 
     elif fs == 16000:
-        return srmrpy.srmr(speech, fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
-    
+        srmRatio,energy=srmrpy.srmr(speech, fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
+        return srmRatio
     else:
         numSamples=round(len(speech)/fs*16000)
         fs = 16000
-        return srmrpy.srmr(resample(speech, numSamples), fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
+        srmRatio,energy=srmrpy.srmr(resample(speech, numSamples), fs, n_cochlear_filters=n_cochlear_filters, low_freq=low_freq, min_cf=min_cf, max_cf=max_cf, fast=fast, norm=norm)
+        return srmRatio 
 
 
 def hz_to_bark(freqs_hz):
